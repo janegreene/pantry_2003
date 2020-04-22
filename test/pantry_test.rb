c1cddad1cd2b71398e5ecb8b0fe605cd59896f93
Jane Greene <janegreenecasa@gmail.com>
2020-04-22 09:28:17 -0600

    added tests for pantry

---
+++ test/pantry_test.rb
@@ -0,0 +1,33 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/ingredient'
+require './lib/pantry'
+
+class PantryTest < Minitest::Test
+  def setup
+    @pantry = Pantry.new
+    @ingredient1 = Ingredient.new({name: "Cheese", unit: "oz", calories: 50})
+    # @ingredient2 = Ingredient.new("Macaroni", "oz", 200)
+  end
+
+  def test_it_exists
+    assert_instance_of Pantry, @pantry
+  end
+
+  def test_it_has_attributes
+    assert_equal ({}), @pantry.stock
+  end
+
+  def test_it_can_stock_check
+    assert_equal 0, @pantry.stock_check(@ingredient1)
+  end
+
+  def test_it_can_restock
+    @pantry.restock(@ingredient1, 5)
+    @pantry.restock(@ingredient1, 10)
+    assert_equal 15, @pantry.stock_check(@ingredient1)
+    @pantry.restock(@ingredient2, 7)
+    assert_equal 7, @pantry.stock_check(@ingredient2)
+  end
+
+end
