10496d23cf314c742af97bed57c39ef230f525d1
Jane Greene <janegreenecasa@gmail.com>
2020-04-22 10:29:52 -0600

    added new test for recipe total calories

--- test/recipe_test.rb
+++ test/recipe_test.rb
@@ -3,11 +3,16 @@ require 'minitest/pride'
 require './lib/ingredient'
 require './lib/pantry'
 require './lib/recipe'
+require './lib/cook_book'
 
 class RecipeTest < Minitest::Test
   def setup
+    @pantry = Pantry.new
+    @cookbook = CookBook.new
     @ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
     @ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+    @ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+    @ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
     @recipe1 = Recipe.new("Mac and Cheese")
     @recipe2 = Recipe.new("Cheese Burger")
   end
@@ -34,7 +39,60 @@ class RecipeTest < Minitest::Test
     @recipe1.add_ingredient(@ingredient2, 8)
     assert_equal [@ingredient1, @ingredient2], @recipe1.ingredients
   end
+
+  def test_it_can_return_total_calories
+    @recipe1.add_ingredient(@ingredient1, 2)
+    @recipe1.add_ingredient(@ingredient2, 8)
+    @recipe2.add_ingredient(@ingredient1, 2)
+    @recipe2.add_ingredient(@ingredient3, 4)
+    @recipe2.add_ingredient(@ingredient4, 1)
+    assert_equal 440, @recipe1.total_calories
+    assert_equal 675, @recipe2.total_calories
+  end
+  
 end
+# pry(main)> pantry = Pantry.new
+# # => #<Pantry:0x007fd8858863b8...>
+#
+# pry(main)> cookbook = CookBook.new
+# # => #<CookBook:0x00007faae6a42228 @recipes=[]>
+#
+# pry(main)> ingredient1 = Ingredient.new({name: "Cheese", unit: "C", calories: 100})
+# # => #<Ingredient:0x00007faae6a207e0...>
+#
+# pry(main)> ingredient2 = Ingredient.new({name: "Macaroni", unit: "oz", calories: 30})
+# # => #<Ingredient:0x00007faae69e3cf0...>
+#
+# pry(main)> recipe1 = Recipe.new("Mac and Cheese")
+# # => #<Recipe:0x00007faae69c9698...>
+#
+# pry(main)> recipe1.add_ingredient(ingredient1, 2)
+#
+# pry(main)> recipe1.add_ingredient(ingredient2, 8)
+#
+# pry(main)> ingredient3 = Ingredient.new({name: "Ground Beef", unit: "oz", calories: 100})
+# # => #<Ingredient:0x00007faae6950860...>
+#
+# pry(main)> ingredient4 = Ingredient.new({name: "Bun", unit: "g", calories: 75})
+# # => #<Ingredient:0x00007faae694bb80...>
+#
+# pry(main)> recipe2 = Recipe.new("Cheese Burger")
+# # => #<Recipe:0x00007faae692a110...>
+#
+# pry(main)> recipe2.add_ingredient(ingredient1, 2)
+#
+# pry(main)> recipe2.add_ingredient(ingredient3, 4)
+#
+# pry(main)> recipe2.add_ingredient(ingredient4, 1)
+#
+# pry(main)> recipe1.total_calories
+# # => 440
+#
+# pry(main)> recipe2.total_calories
+# # => 675
+
+######################################
+
 ## Iteration 2 - Recipe and CookBook
 
 # ```markdown
