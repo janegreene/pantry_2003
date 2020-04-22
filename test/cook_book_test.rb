c9917277faeb8d94a456c9c405e28cd887ba5ccf
Jane Greene <janegreenecasa@gmail.com>
2020-04-22 10:12:06 -0600

    completed tests for cookbook

--- test/cook_book_test.rb
+++ test/cook_book_test.rb
@@ -6,7 +6,23 @@ require './lib/recipe'
 require './lib/cookbook'
 
 class CookBookTest < Minitest::Test
+  def setup
+    @cookbook = CookBook.new
+  end
 
+  def test_it_exists
+    assert_instance_of CookBook, @cookbook
+  end
+
+  def test_it_has_attributes
+    assert_equal [], @cookbook.recipes
+  end
+
+  def test_it_can_add_recipe
+    @cookbook.add_recipe(@recipe1)
+    @cookbook.add_recipe(@recipe2)
+    assert_equal [@recipe1, @recipe2], @cookbook.recipes
+  end
 end
 # pry(main)> cookbook = CookBook.new
 # # => #<CookBook:0x00007faae6a42228 @recipes=[]>
