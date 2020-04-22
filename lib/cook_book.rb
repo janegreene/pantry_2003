dabd31ed55b177b31b5e14a4fc7ca1d5a53bcf81
Jane Greene <janegreenecasa@gmail.com>
2020-04-22 11:02:59 -0600

    added method for ingredients

--- lib/cook_book.rb
+++ lib/cook_book.rb
@@ -11,6 +11,12 @@ class CookBook
   end
 
   def ingredients
-    require "pry"; binding.pry
+    list = []
+    @recipes.each do |recipe|
+      recipe.ingredients_required.each do |k, v|
+          list << k.name
+      end
+    end
+      list.uniq
   end
 end
